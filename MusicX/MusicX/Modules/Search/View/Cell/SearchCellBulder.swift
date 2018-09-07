665f4c13970aed63af2d850da410179b9a174c39
Ilya Tkachou <Ilya_Tkachou@epam.com>
2018-09-07 17:22:53 +0300

    Download cell created.

--- MusicX/MusicX/Modules/Search/View/Cell/SearchCellBulder.swift
+++ MusicX/MusicX/Modules/Search/View/Cell/SearchCellBulder.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class SearchCellBuilder : BaseCellBuilder {
     func build<T>(cell: UITableViewCell, data: T) {
+        guard let cellData = data as? SearchCellData else {
+            return
+        }
+        
+        switch cellData.type {
+        case .track:
+            guard let trackCell = cell as? SearchTrackCell, let trackData = cellData.media as? Download else {
+                return
+            }
+            setupDownloadCell(cell: trackCell , track: trackData)
+        case .artist:
+            return
+        case .tag:
+            return
+        }
+    }
+    
+    class SearchCellData {
+        let type: MediaType
+        let media: BaseMediaObject
+        
+        init(type: MediaType, media: BaseMediaObject) {
+            self.type = type
+            self.media = media
+        }
+    }
+    
+    private func setupDownloadCell(cell: SearchTrackCell, download: Download) {
         
     }
 }
---
+++ MusicX/MusicX/Modules/Search/View/Cell/Track/SearchTrackCell.swift
@@ -0,0 +1,15 @@
+//
+//  SearchTrackCell.swift
+//  MusicX
+//
+//  Created by Ilya Tkachou on 9/7/18.
+//  Copyright © 2018 Ilya Tkachou. All rights reserved.
+//
+
+import UIKit
+
+class SearchTrackCell: UITableViewCell, BaseTableCell {
+    static var identifier = "SearchTrackCell"
+    
+    
+}
