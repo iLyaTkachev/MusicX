fe20d81a27a24cfcf93d34921cc436644b4c5732
Ilya Tkachou <Ilya_Tkachou@epam.com>
2018-09-17 11:37:07 +0300

    Before deleting default core data implementation.

---
+++ MusicX/MusicX/DataManager/Local/DB/BaseCoreDataManager.swift
@@ -0,0 +1,15 @@
+//
+//  BaseCoreDataManager.swift
+//  MusicX
+//
+//  Created by Ilya Tkachou on 9/17/18.
+//  Copyright © 2018 Ilya Tkachou. All rights reserved.
+//
+
+import Foundation
+
+protocol BaseCoreDataManager {
+    func fetch()
+    func create()
+    func update()
+}
---
+++ MusicX/MusicX/DataManager/Local/DB/CoreDataManager.swift
@@ -0,0 +1,23 @@
+//
+//  CoreDataManager.swift
+//  MusicX
+//
+//  Created by Ilya Tkachou on 9/17/18.
+//  Copyright © 2018 Ilya Tkachou. All rights reserved.
+//
+
+import Foundation
+
+class CoreDataManager: BaseCoreDataManager {
+    func fetch() {
+        
+    }
+    
+    func create() {
+        
+    }
+    
+    func update() {
+        
+    }
+}
